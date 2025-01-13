a3de88752a0b28c183f90699d61230500016a081
allie1998 <87417361+allie1998@users.noreply.github.com>
2025-01-13 19:25:01 +0000

    Deploying to gh-pages from @ ualberta-vision-robotics/cmput428labs@7d05528b6d5c2924565c5414d7641dbbc55193f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,38 +16,66 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cmput428labs/policies";
           },
-        },{id: "nav-labs",
-          title: "Labs",
-          description: "Here you&#39;ll find the resources for each lab assignment, separated by topic.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cmput428labs/labs/";
-          },
-        },{id: "projects-lab-1-1-optical-flow",
+        },{id: "dropdown-lab-1-1",
+              title: "Lab 1.1",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-lab-1-2",
+              title: "Lab 1.2",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-lab-2-1",
+              title: "Lab 2.1",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-lab-2-2",
+              title: "Lab 2.2",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-lab-3",
+              title: "Lab 3",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "projects-lab-1-1-optical-flow",
           title: 'Lab 1.1 Optical Flow',
           description: "Implementing basic optical flow for motion estimation.",
           section: "Projects",handler: () => {
-              window.location.href = "/cmput428labs/labs/opticalflow";
+              window.location.href = "/cmput428labs/opticalflow";
             },},{id: "projects-lab-1-2-tracking",
           title: 'Lab 1.2 - Tracking',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/cmput428labs/labs/tracking";
+              window.location.href = "/cmput428labs/tracking";
             },},{id: "projects-lab-2-1-2d-projective-geometry-and-homography-estimation",
           title: 'Lab 2.1 - 2D Projective Geometry and Homography Estimation',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/cmput428labs/labs/2dgeometry";
+              window.location.href = "/cmput428labs/2dgeometry";
             },},{id: "projects-lab-2-2-3d-projective-geometry-and-stereo-reconstruction",
           title: 'Lab 2.2 - 3D Projective Geometry and Stereo Reconstruction',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/cmput428labs/labs/3dgeometry";
+              window.location.href = "/cmput428labs/3dgeometry";
             },},{id: "projects-lab-3-structure-from-motion",
           title: 'Lab 3 - Structure from Motion',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/cmput428labs/labs/structurefrommotion";
+              window.location.href = "/cmput428labs/structurefrommotion";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
